72e85b0dbd29f1991e12058df28c43f0f85e5476
neverxie <neverxie@outlook.com>
2018-05-31 16:28:48 +0800

    .
    5月31代码大更新

--- bsp/imxrt1052-evk/RTE/_RT-Thread_IMXRT1052/RTE_Components.h
+++ bsp/imxrt1052-evk/RTE/_RT-Thread_IMXRT1052/RTE_Components.h
@@ -3,7 +3,7 @@
  * Auto generated Run-Time-Environment Component Configuration File
  *      *** Do not modify ! ***
  *
- * Project: 'project' 
+ * Project: 'template' 
  * Target:  'RT-Thread IMXRT1052' 
  */
 
