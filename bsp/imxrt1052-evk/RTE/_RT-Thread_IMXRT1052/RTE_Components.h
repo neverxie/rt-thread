62d58fa4ac8dc9bac82f4538f6b58c82f8dfef1a
neverxie <neverxie@outlook.com>
2018-05-31 22:46:39 +0800

    .

--- bsp/imxrt1052-evk/RTE/_RT-Thread_IMXRT1052/RTE_Components.h
+++ bsp/imxrt1052-evk/RTE/_RT-Thread_IMXRT1052/RTE_Components.h
@@ -3,7 +3,7 @@
  * Auto generated Run-Time-Environment Component Configuration File
  *      *** Do not modify ! ***
  *
- * Project: 'template' 
+ * Project: 'project' 
  * Target:  'RT-Thread IMXRT1052' 
  */
 
