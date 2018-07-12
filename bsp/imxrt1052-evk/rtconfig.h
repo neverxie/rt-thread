03bdcfb4dae3e008429e650ac2332b75ea58eb8f
neverxie <neverxie@outlook.com>
2018-07-12 10:19:18 +0800

    更新如下： 1、开启DHCP 2、修改断言bug

--- bsp/imxrt1052-evk/rtconfig.h
+++ bsp/imxrt1052-evk/rtconfig.h
@@ -119,6 +119,9 @@
 #define RT_USING_LWIP202
 #define RT_LWIP_ICMP
 #define RT_LWIP_DNS
+#define RT_LWIP_DHCP
+#define IP_SOF_BROADCAST 1
+#define IP_SOF_BROADCAST_RECV 1
 
 /* Static IPv4 Address */
 
